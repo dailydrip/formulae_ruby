1bbd2307ee2fc2cdd3c7637e2c495a5cf4cabd51
Franzé Jr <franzejr@gmail.com>
2017-06-20 17:05:38 -0300

    Update resources to work with The Formulae Example

--- lib/formulae_ruby/resources/form.rb
+++ lib/formulae_ruby/resources/form.rb
@@ -1,6 +1,6 @@
 module FormulaeRuby
   class Form
-    attr_accessor :id, :application_id, :sections, :questions
+    attr_accessor :id, :application_id, :completion_content, :sections, :questions
 
     def initialize(json)
       decode_form(json)
@@ -9,23 +9,24 @@ module FormulaeRuby
     private def decode_form(json)
       self.id = json['id']
       self.application_id = json['application']['id']
-      self.sections = add_sections(json)
-      self.questions = add_questions(json)
+      self.completion_content = json['completion_content']
+      self.sections = add_sections(json['sections']) if json['sections']
+      self.questions = add_questions(json['questions']) if json['questions']
       self
     end
 
     private def add_sections(json)
       [].tap do |s|
-        json['sections'].each do |section_hash|
+        json.each do |section_hash|
           s << Section.new(section_hash)
         end
       end
     end
 
     private def add_questions(json)
-      [].tap do |q|
-        json['questions'].each do |question_hash|
-          q << Question.new(question_hash)
+      [].tap do |questions|
+        json.each do |question_hash|
+          questions << Question.new(question_hash)
         end
       end
     end
